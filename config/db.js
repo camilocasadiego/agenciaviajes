dd9cb3568838c2678d7d53638fa6d6de96c4a60c
Camilo Casadiego <camilocasadiego31@gmail.com>
2024-09-22 20:20:26 +0200

    first commit

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: '3306',
-    dialect: 'mysql',
+const db = new Sequelize(process.env.DB_URL, {
     define: {
         timestamps: false
     },
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ db.authenticate()
     .catch( error => console.log(error));
 
 // Definir puerto
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 // Habilitar PUG
 app.set('view engine', 'pug');
