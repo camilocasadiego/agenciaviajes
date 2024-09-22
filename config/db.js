5da242862eb979862c8622a425f52ca8d70d4687
Camilo Casadiego <camilocasadiego31@gmail.com>
2024-09-22 20:42:42 +0200

    Problemas de conexión con la base de datos

--- config/db.js
+++ config/db.js
@@ -2,8 +2,11 @@
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
-console.log(process.env.DB_URL);
-const db = new Sequelize(process.env.DB_URL, {
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
     define: {
         timestamps: false
     },
